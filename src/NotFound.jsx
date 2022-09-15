c8a785f96af548c4d6bb0d05a592561b665d2825
Rodolfo <rodoviario@users.noreply.github.com>
2022-09-15 12:00:23 +0000

    Initial commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,15 @@
+const NotFound = ({ selectedTeam, teamMemberCount }) => {
+
+  return (
+    <header className = "container">
+      <div className = "row justify-content-center mt-3 mb-4">
+        <div className = "col-8">
+          <h1 class="text-danger">404 - Page not found</h1>
+        </div>
+      </div>
+    </header>
+  )
+
+}
+
+export default NotFound
